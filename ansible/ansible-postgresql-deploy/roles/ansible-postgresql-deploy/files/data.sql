f9be1042492480d7c7eacea1df6d6da0431193a8
OHAW <ouhayw@gmail.com>
2023-04-13 00:25:59 +0400

    push to repo

---
+++ ansible/ansible-postgresql-deploy/roles/ansible-postgresql-deploy/files/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO test_data (numder, somewords) VALUES (1, 'one');
+INSERT INTO test_data (numder, somewords) VALUES (2, 'two');
+INSERT INTO test_data (numder, somewords) VALUES (3, 'three');
+INSERT INTO test_data (numder, somewords) VALUES (4, 'four');
+INSERT INTO test_data (numder, somewords) VALUES (5, 'five');
+INSERT INTO test_data (numder, somewords) VALUES (6, 'six');
+INSERT INTO test_data (numder, somewords) VALUES (7, 'seven');
+INSERT INTO test_data (numder, somewords) VALUES (8, 'eight');
